312de2d94f184807237196faddcbb3ab2c65ce31
yeeway <yiwei.suuu@gmail.com>
2025-01-02 21:55:35 +0800

    feat: implement Spotify authentication with NextAuth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import authOptions from "@/app/api/auth/[...nextauth]/authOptions";
+import AuthSessionProvider from "@/components/AuthSessionProvider";
+import { getServerSession } from "next-auth";
 
 export const metadata: Metadata = {
   title: "Spotify Artist2Playlist",
   description: "Add all songs of artist on Spotify to your playlists.",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
-      <body className="antialiased">{children}</body>
+      <AuthSessionProvider session={session}>
+        <body className="antialiased">{children}</body>
+      </AuthSessionProvider>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+// import { useEffect, useState } from "react";
+import { useSession, signOut, signIn } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const session = useSession();
+
+  if (!session || session.status !== "authenticated") {
+    return (
+      <div>
+        <h1>Spotify Web API Typescript SDK in Next.js</h1>
+        <Button onClick={() => signIn("spotify")}>Sign in with Spotify</Button>
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-dvh">
       <main className="">
-
+        <p>Logged in as {session.data.user?.name}</p>
+        <button onClick={() => signOut()}>Sign out</button>
       </main>
-      <footer className="">
-
-      </footer>
+      <footer className=""></footer>
     </div>
   );
 }
