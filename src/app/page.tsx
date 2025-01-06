7ef8cef3777d0131f574d8d0b4172928f806bcef
yeeway <yiwei.suuu@gmail.com>
2025-01-06 23:09:55 +0800

    fix: update CSS variable precision and adjust Lottie animation source based on theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { DotLottieWorker, DotLottieWorkerReact } from '@lottiefiles/dotlottie-react'
 import { Artist, SimplifiedPlaylist, SimplifiedTrack } from '@spotify/web-api-ts-sdk'
 import { useSession, signOut, signIn } from 'next-auth/react'
+import { useTheme } from 'next-themes'
 import SelectArtist from '@/components/SelectArtist'
 import SelectPlaylist from '@/components/SelectPlaylist'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
@@ -13,11 +14,14 @@ import { addTracksToPlaylist, getAlbumsFromArtist, getTracksFromAlbum } from '@/
 export default function App() {
   // TODO: session 過期要導回登入頁
   const session = useSession()
+  const { resolvedTheme } = useTheme()
   const [selectedArtist, setSelectedArtist] = useState<Artist | null>(null)
   const [selectedPlaylist, setSelectedPlaylist] = useState<SimplifiedPlaylist | null>(null)
   const [status, setStatus] = useState<'idle' | 'processing' | 'done'>('idle')
   const [arrowLottie, setArrowLottie] = useState<DotLottieWorker | null>(null)
   const [progressAlbum, setProgressAlbum] = useState('')
+  const LOTTIE_URL_WHITE = 'https://lottie.host/e0a7567a-3fd4-401f-80b7-52f41c8a8b7d/trvhjG7OJ0.lottie'
+  const LOTTIE_URL_BLACK = 'https://lottie.host/1533e124-3390-4754-93cc-c08bcecbb0d7/AzwvLr5fRz.lottie'
 
   async function getAllTracksFromArtist(id: string): Promise<SimplifiedTrack[]> {
     try {
@@ -78,7 +82,7 @@ export default function App() {
         <DotLottieWorkerReact
           className="mx-auto my-2 h-20 w-[130px]"
           dotLottieRefCallback={setArrowLottie}
-          src="https://lottie.host/1533e124-3390-4754-93cc-c08bcecbb0d7/AzwvLr5fRz.lottie"
+          src={resolvedTheme === 'dark' ? LOTTIE_URL_WHITE : LOTTIE_URL_BLACK}
           loop
         />
 
